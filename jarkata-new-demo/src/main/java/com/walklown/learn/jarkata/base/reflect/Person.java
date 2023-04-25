7dd1859c23c8e1a0c3233d055af02b41f9507c43
walklown <walklown@gmail.com>
2023-04-26 02:07:39 +0800

    create reflect demo

---
+++ jarkata-new-demo/src/main/java/com/walklown/learn/jarkata/base/reflect/Person.java
@@ -0,0 +1,49 @@
+package com.walklown.learn.jarkata.base.reflect;
+
+public class Person {
+
+    private String name;
+
+    private String mobile;
+
+    public Person() {
+    }
+
+    public Person(String name, String mobile) {
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", mobile='" + mobile + '\'' +
+                '}';
+    }
+
+    private void sleep() {
+        System.out.println("sleeping");
+    }
+
+    public static String transfer(String param) {
+        System.out.println("params:" + param);
+        return param;
+    }
+}
---
+++ jarkata-new-demo/src/main/java/com/walklown/learn/jarkata/base/reflect/ReflectDemo.java
@@ -0,0 +1,13 @@
+package com.walklown.learn.jarkata.base.reflect;
+
+/*
+ * https://blog.csdn.net/ShuSheng0007/article/details/107066856
+ *
+ * @author walklown
+ */
+public class ReflectDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
