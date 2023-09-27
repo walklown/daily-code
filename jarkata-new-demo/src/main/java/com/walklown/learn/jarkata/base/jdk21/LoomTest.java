5f3fd2cc6e5f755eec4e6a8d61f2bf8ccdeac496
walklown <walklown@gmail.com>
2023-09-27 09:55:05 +0800

    增加jdk21的demo

---
+++ jarkata-new-demo/src/main/java/com/walklown/learn/jarkata/base/jdk21/LoomTest.java
@@ -0,0 +1,61 @@
+package com.walklown.learn.jarkata.base.jdk21;
+
+
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.stream.IntStream;
+
+class LoomTest {
+
+    public static void main(String[] args) throws Exception {
+        var observed = new ConcurrentSkipListSet<String>();
+
+        var threads = IntStream
+                .range(0, 100)
+                .mapToObj(index -> Thread.ofVirtual() // ①
+                        .unstarted(() -> {
+                            var first = index == 0;
+                            if (first) {
+                                observed.add(Thread.currentThread().toString());
+                            }
+                            try {
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                            if (first) {
+                                observed.add(Thread.currentThread().toString());
+                            }
+                            try {
+                                Thread.sleep(20);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                            if (first) {
+                                observed.add(Thread.currentThread().toString());
+                            }
+                            try {
+                                Thread.sleep(20);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                            if (first) {
+                                observed.add(Thread.currentThread().toString());
+                            }
+                        }))
+                .toList();
+
+        for (var t : threads) {
+            t.start();
+        }
+
+        for (var t : threads) {
+            t.join();
+        }
+
+        for (String s : observed) {
+            System.out.println(s);
+        }
+
+    }
+
+}
