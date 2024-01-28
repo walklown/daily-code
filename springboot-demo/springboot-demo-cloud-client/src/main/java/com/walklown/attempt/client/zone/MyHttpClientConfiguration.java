7e93c281315ee14bd54582e923e5c0290ea81772
walklown <walklown@gmail.com>
2024-01-28 18:17:05 +0800

    merge pf

--- springboot-demo/springboot-demo-cloud-client/src/main/java/com/walklown/attempt/client/zone/MyHttpClientConfiguration.java
+++ springboot-demo/springboot-demo-cloud-client/src/main/java/com/walklown/attempt/client/zone/MyHttpClientConfiguration.java
@@ -1,24 +1,16 @@
 package com.walklown.attempt.client.zone;
 
-import com.walklown.attempt.zone.HttpClientConfiguration;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClientSpecification;
-import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
-
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnClass(LoadBalancerClientFactory.class)
-public class MyHttpClientConfiguration implements InitializingBean {
+//@ConditionalOnClass(LoadBalancerClientFactory.class)
+public class MyHttpClientConfiguration {
 
-    @Autowired
-    private LoadBalancerClientFactory loadBalancerClientFactory;
+//    @Autowired
+//    private LoadBalancerClientFactory loadBalancerClientFactory;
 
     @LoadBalanced
     @Bean
@@ -26,8 +18,8 @@ public class MyHttpClientConfiguration implements InitializingBean {
         return new RestTemplate();
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        loadBalancerClientFactory.setConfigurations(Collections.singletonList(new LoadBalancerClientSpecification("default.my-http-client", new Class[]{HttpClientConfiguration.class})));
-    }
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        loadBalancerClientFactory.setConfigurations(Collections.singletonList(new LoadBalancerClientSpecification("default.my-http-client", new Class[]{HttpClientConfiguration.class})));
+//    }
 }
--- springboot-demo/springboot-demo-cloud-client/src/main/java/com/walklown/attempt/zone/HttpClientConfiguration.java
+++ springboot-demo/springboot-demo-cloud-client/src/main/java/com/walklown/attempt/zone/HttpClientConfiguration.java
@@ -1,98 +1,98 @@
-package com.walklown.attempt.zone;
-
-import io.microsphere.multiple.active.zone.ZoneContext;
-import io.microsphere.multiple.active.zone.ZonePreferenceFilter;
-import io.microsphere.multiple.active.zone.cloud.CloudServerZoneResolver;
-import io.microsphere.multiple.active.zone.spring.cloud.loadbalancer.ZonePreferenceServiceInstanceListSupplier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.cloud.client.ConditionalOnBlockingDiscoveryEnabled;
-import org.springframework.cloud.client.ConditionalOnReactiveDiscoveryEnabled;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClientConfiguration;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClientSpecification;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
-import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
-import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
-import org.springframework.cloud.loadbalancer.support.LoadBalancerEnvironmentPropertyUtils;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.retry.support.RetryTemplate;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Collections;
-
-/**
- * http配置
- *
- * @author <a href="mailto:warlklown@gmail.com">Walklown<a/>
- * @see LoadBalancerClientConfiguration.BlockingSupportConfiguration
- */
-@Configuration(proxyBeanMethods = false)
-@LoadBalancerClients
-//@Import(value = ZonePreferenceLoadBalancerClientConfiguration.class)
-public class HttpClientConfiguration {
-
-    @ConditionalOnReactiveDiscoveryEnabled
-    @Order(193827465)
-    static class ReactiveConfiguration {
-
-        @Bean
-        @ConditionalOnBean(DiscoveryClient.class)
-        @ConditionalOnMissingBean
-        @Conditional(MyZoneConfigurationCondition.class)
-        public ServiceInstanceListSupplier zonePreferenceServiceInstanceListSupplier(
-                ConfigurableApplicationContext context,
-                ZonePreferenceFilter<ServiceInstance> zonePreferenceFilter) {
-            return ServiceInstanceListSupplier.builder().withDiscoveryClient().with(
-                    (ctx, delegate) -> new ZonePreferenceServiceInstanceListSupplier(delegate, zonePreferenceFilter)
-            ).build(context);
-        }
-    }
-
-
-    @ConditionalOnBlockingDiscoveryEnabled
-    @Order(193827466)
-    static class BlockingConfiguration {
-
-        @Bean
-        @ConditionalOnBean(DiscoveryClient.class)
-        @ConditionalOnMissingBean
-        @Conditional(MyZoneConfigurationCondition.class)
-        public ServiceInstanceListSupplier zonePreferenceServiceInstanceListSupplier(
-                ConfigurableApplicationContext context,
-                ZonePreferenceFilter<ServiceInstance> zonePreferenceFilter) {
-            return ServiceInstanceListSupplier.builder().withBlockingDiscoveryClient().with(
-                    (ctx, delegate) -> new ZonePreferenceServiceInstanceListSupplier(delegate, zonePreferenceFilter)
-            ).build(context);
-        }
-    }
-
-    @Bean
-    public ZonePreferenceFilter<ServiceInstance> zonePreferenceFilter(ZoneContext zoneContext) {
-        zoneContext.setPreferenceEnabled(true);
-        zoneContext.setEnabled(true);
-        zoneContext.setPreferenceUpstreamSameZoneMinAvailable(1);
-        return new ZonePreferenceFilter<ServiceInstance>(zoneContext, new CloudServerZoneResolver());
-    }
-
-
-    static class MyZoneConfigurationCondition implements Condition {
-
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return LoadBalancerEnvironmentPropertyUtils.equalToForClientOrDefault(context.getEnvironment(),
-                    "configurations", "my-zone");
-        }
-
-    }
-}
+//package com.walklown.attempt.zone;
+//
+//import io.microsphere.multiple.active.zone.ZoneContext;
+//import io.microsphere.multiple.active.zone.ZonePreferenceFilter;
+//import io.microsphere.multiple.active.zone.cloud.CloudServerZoneResolver;
+//import io.microsphere.multiple.active.zone.spring.cloud.loadbalancer.ZonePreferenceServiceInstanceListSupplier;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+//import org.springframework.cloud.client.ConditionalOnBlockingDiscoveryEnabled;
+//import org.springframework.cloud.client.ConditionalOnReactiveDiscoveryEnabled;
+//import org.springframework.cloud.client.ServiceInstance;
+//import org.springframework.cloud.client.discovery.DiscoveryClient;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClientConfiguration;
+//import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClientSpecification;
+//import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
+//import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+//import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+//import org.springframework.cloud.loadbalancer.support.LoadBalancerEnvironmentPropertyUtils;
+//import org.springframework.context.ConfigurableApplicationContext;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Condition;
+//import org.springframework.context.annotation.ConditionContext;
+//import org.springframework.context.annotation.Conditional;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.core.type.AnnotatedTypeMetadata;
+//import org.springframework.retry.support.RetryTemplate;
+//import org.springframework.web.client.RestTemplate;
+//
+//import java.util.Collections;
+//
+///**
+// * http配置
+// *
+// * @author <a href="mailto:warlklown@gmail.com">Walklown<a/>
+// * @see LoadBalancerClientConfiguration.BlockingSupportConfiguration
+// */
+//@Configuration(proxyBeanMethods = false)
+//@LoadBalancerClients
+////@Import(value = ZonePreferenceLoadBalancerClientConfiguration.class)
+//public class HttpClientConfiguration {
+//
+//    @ConditionalOnReactiveDiscoveryEnabled
+//    @Order(193827465)
+//    static class ReactiveConfiguration {
+//
+//        @Bean
+//        @ConditionalOnBean(DiscoveryClient.class)
+//        @ConditionalOnMissingBean
+//        @Conditional(MyZoneConfigurationCondition.class)
+//        public ServiceInstanceListSupplier zonePreferenceServiceInstanceListSupplier(
+//                ConfigurableApplicationContext context,
+//                ZonePreferenceFilter<ServiceInstance> zonePreferenceFilter) {
+//            return ServiceInstanceListSupplier.builder().withDiscoveryClient().with(
+//                    (ctx, delegate) -> new ZonePreferenceServiceInstanceListSupplier(delegate, zonePreferenceFilter)
+//            ).build(context);
+//        }
+//    }
+//
+//
+//    @ConditionalOnBlockingDiscoveryEnabled
+//    @Order(193827466)
+//    static class BlockingConfiguration {
+//
+//        @Bean
+//        @ConditionalOnBean(DiscoveryClient.class)
+//        @ConditionalOnMissingBean
+//        @Conditional(MyZoneConfigurationCondition.class)
+//        public ServiceInstanceListSupplier zonePreferenceServiceInstanceListSupplier(
+//                ConfigurableApplicationContext context,
+//                ZonePreferenceFilter<ServiceInstance> zonePreferenceFilter) {
+//            return ServiceInstanceListSupplier.builder().withBlockingDiscoveryClient().with(
+//                    (ctx, delegate) -> new ZonePreferenceServiceInstanceListSupplier(delegate, zonePreferenceFilter)
+//            ).build(context);
+//        }
+//    }
+//
+//    @Bean
+//    public ZonePreferenceFilter<ServiceInstance> zonePreferenceFilter(ZoneContext zoneContext) {
+//        zoneContext.setPreferenceEnabled(true);
+//        zoneContext.setEnabled(true);
+//        zoneContext.setPreferenceUpstreamSameZoneMinAvailable(1);
+//        return new ZonePreferenceFilter<ServiceInstance>(zoneContext, new CloudServerZoneResolver());
+//    }
+//
+//
+//    static class MyZoneConfigurationCondition implements Condition {
+//
+//        @Override
+//        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+//            return LoadBalancerEnvironmentPropertyUtils.equalToForClientOrDefault(context.getEnvironment(),
+//                    "configurations", "my-zone");
+//        }
+//
+//    }
+//}
